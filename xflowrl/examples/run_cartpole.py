1fc7f3a746fe8a5af36c3a833ac6dcc8a03b8304
Sean Parker <sean.parker067@gmail.com>
2021-01-27 01:23:45 +0000

    Small cartpole hyperparam changes

--- xflowrl/examples/run_cartpole.py
+++ xflowrl/examples/run_cartpole.py
@@ -87,14 +87,14 @@ def main(argv):
     num_actions = env.action_space.n
     agent = Agent(
         num_actions=num_actions,
-        discount=0.99,
-        gae_lambda=1.0,
+        discount=0.95,
+        gae_lambda=0.97,
         reducer=tf.math.unsorted_segment_sum,
         # Typically use small learning rates, depending on problem try [0.0025 - 0.00001]
         # I did not tune them particularly for this toy problem.
         learning_rate=0.0025,
         # Value function can have the same or a slightly more aggressive learning rate.
-        baseline_learning_rate=0.0025,
+        baseline_learning_rate=0.03,
         policy_layer_size=32,
         # This limits the aggressiveness of the update -> 0.2 is often the default value, 0.3
         # for a more aggressive update, 0.1 for a more conservative one.
