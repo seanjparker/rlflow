6c569f3aa1c9f263c726ebb92bb36a3d8c33d713
Sean Parker <sean.parker067@gmail.com>
2021-04-14 19:12:12 +0100

    Fix flaky checkpoint restarts

--- xflowrl/agents/mb_agent.py
+++ xflowrl/agents/mb_agent.py
@@ -80,7 +80,7 @@ class MBAgent(_BaseAgent):
             checkpoint_root += f'/{network_name}'
         if checkpoint_timestamp is not None:
             checkpoint_root += f'/{checkpoint_timestamp}'
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), trunk=self.trunk)
+        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1), trunk=self.trunk, trunk_optimizer=self.trunk_optimizer)
         self.ckpt_manager = tf.train.CheckpointManager(self.ckpt, checkpoint_root, max_to_keep=5)
 
     def act(self, states: Union[dict, list], explore=True):
